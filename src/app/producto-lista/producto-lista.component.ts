7555b2ac773bbfe3fb6e2455c51751aa70c607fc
Carlos <carlossabrido12@gmail.com>
2023-06-27 16:43:07 +0200

    get product function

--- src/app/producto-lista/producto-lista.component.ts
+++ src/app/producto-lista/producto-lista.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Producto } from '../producto';
+import { ProductoService } from '../producto.service';
 
 @Component({
   selector: 'app-producto-lista',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   
 })
 export class ProductoListaComponent {
+    productos: Producto[];
+
+    constructor (private productoServicio: ProductoService){}
+
+    ngOnInit(){
+      // cargamos los productos 
+      this.obtenerProductos()
+    }
+
+    private obtenerProductos(){
+      // consumir los datos del observable 
+      this.productoServicio.obtenerProductoLista().subscribe(
+        (datos=>{
+        this.productos= datos;
+      })
+      )
+    }
+
 
 }
